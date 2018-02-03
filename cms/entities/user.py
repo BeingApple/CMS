b2c1c8e42ae85f0a9d38e5ed5c56a82d8901bc96
Unknown <whdydql123@naver.com>
2018-02-03 16:30:27 +0900

    entity User 추가
    entity User 추가

---
+++ cms/entities/user.py
@@ -0,0 +1,27 @@
+from cms.database import db
+from sqlalchemy_utils import UUIDType
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+
+    id = db.Column(UUIDType(binary=False), primary_key=True)
+    gender = db.Column(db.Text)
+    age = db.Column(db.Numeric)
+    ages = db.Column(db.Numeric)
+    phone = db.Column(db.Text)
+    birthdate = db.Column(db.Date)
+
+    def __init__(self, id=None, gender=None, age=None, ages=None,
+                 phone=None, birthdate=None):
+        self.id = id
+        self.gender = gender
+        self.age = age
+        self.ages = ages
+        self.phone = phone
+        self.birthdate = birthdate
+
+    def __repr__(self):
+        return "<User %r>" % self.id
+
+
