7b22e68a0d4ffe01d13a5604a3d20ee26852fd00
Unknown <whdydql123@naver.com>
2018-02-03 20:39:47 +0900

    customer_list 라우팅 추가
    customer_list 라우팅 추가하고 작성한 ServiceUser 에서 pagination, 고객 목록 반환 받아 templates에 출력하도록 라우팅 추가

--- cms/app.py
+++ cms/app.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 from cms.create_app import get_app
+from cms.service.service_user import ServiceUser
 
 
 app = get_app()
@@ -15,4 +16,19 @@ def index():
 
 @app.route("/customer_list/")
 def customer_list():
-    return render_template("customer_list.html")
+    return customer_list_page(0)
+
+
+@app.route("/customer_list/<int:page>/")
+def customer_list_page(page):
+    service_user = ServiceUser()
+
+    users = service_user.get_customer_list(page)
+    pagination = service_user.get_pagination("/customer_list", page)
+
+    return render_template("customer_list.html", users=users,
+                           pagination=pagination)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
