544ab634c83f68d2cfe85aebe8be124ba8f5e92a
Unknown <whdydql123@naver.com>
2018-02-03 20:33:58 +0900

    entity User에 누락 된 column 값 추가
    데이터베이스에 존재 하나 명세서에 누락 된 name Column 데이터를 entity의 user.py 파일에 추가

--- cms/entities/user.py
+++ cms/entities/user.py
@@ -6,15 +6,17 @@ class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(UUIDType(binary=False), primary_key=True)
+    name = db.Column(db.Text)
     gender = db.Column(db.Text)
     age = db.Column(db.Numeric)
     ages = db.Column(db.Numeric)
     phone = db.Column(db.Text)
     birthdate = db.Column(db.Date)
 
-    def __init__(self, id=None, gender=None, age=None, ages=None,
+    def __init__(self, id=None, name=None, gender=None, age=None, ages=None,
                  phone=None, birthdate=None):
         self.id = id
+        self.name = name
         self.gender = gender
         self.age = age
         self.ages = ages
