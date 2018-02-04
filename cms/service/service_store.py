02f0946b1e08e0d48e287f7c568a23cc33194fb9
Unknown <whdydql123@naver.com>
2018-02-04 18:51:58 +0900

    신규 고객 조회 서비스 추가
    해당기간 동안 처음 구매한 신규고객 수를 조회할 수 있는 서비스를 추가하고
    클래스 내부 변수 접근에 대한 방식을 변경하였습니다.

--- cms/service/service_store.py
+++ cms/service/service_store.py
@@ -8,19 +8,22 @@ from cms.entities.order import Order
 
 class ServiceStore:
 
-    retention_count = 0
-    order_count = 0
+    _retention_count = 0
+    _order_count = 0
 
     def get_store_list(self):
         return db.session.query(Store).order_by(Store.name)
 
-    def set_order_count(self, store_id, start_date, end_date):
-        self.order_count = db.session.query(Order). \
+    def _set_order_count(self, store_id, start_date, end_date):
+        self._order_count = db.session.query(Order). \
             filter(Order.store_id == store_id). \
             filter(between(Order.created_at, start_date, end_date)).\
             count()
 
-    def set_retention_count(self, store_id, start_date, end_date):
+    def get_order_count(self):
+        return self._order_count
+
+    def _set_retention_count(self, store_id, start_date, end_date):
         stmt = db.session.query(User, func.count(Order.id).label('cnt')).\
             outerjoin(Order).\
             filter(Order.store_id == store_id).\
@@ -28,14 +31,25 @@ class ServiceStore:
             group_by(User.id).\
             subquery()
 
-        self.retention_count = db.session.query(stmt).\
+        self._retention_count = db.session.query(stmt).\
             select_entity_from(stmt).filter('cnt >= 2').count()
 
     def get_retention_percentage(self, store_id, start_date, end_date):
-        self.set_retention_count(store_id, start_date, end_date)
-        self.set_order_count(store_id, start_date, end_date)
+        self._set_retention_count(store_id, start_date, end_date)
+        self._set_order_count(store_id, start_date, end_date)
 
         try:
-            return round((self.retention_count / self.order_count) * 100, 2)
+            return round((self._retention_count / self._order_count) * 100, 2)
         except ZeroDivisionError:
             return 0
+
+    def get_new_customer_count(self, store_id, start_date, end_date):
+        stmt = db.session.query(User, func.count(Order.id).label('cnt')). \
+            outerjoin(Order). \
+            filter(Order.store_id == store_id). \
+            filter(between(Order.created_at, start_date, end_date)). \
+            group_by(User.id). \
+            subquery()
+
+        return db.session.query(stmt). \
+            select_entity_from(stmt).filter('cnt = 1').count()
