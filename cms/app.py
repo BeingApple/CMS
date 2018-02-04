3e88d13ec35c62af079c202e65f0b97c149aecb7
Unknown <whdydql123@naver.com>
2018-02-04 17:32:07 +0900

    FILTER 처리 방식 변경
    FILTER 적용 후 PAGINATION 시 FILTER 초기화 되는 문제로 인해 POST로 처리하던 FILTER 처리를 GET으로 변경

--- cms/app.py
+++ cms/app.py
@@ -18,12 +18,12 @@ def index():
     return redirect(url_for("customer_list"))
 
 
-@app.route("/customer_list/", methods=['GET', 'POST'])
+@app.route("/customer_list/")
 def customer_list():
     return customer_list_page(0)
 
 
-@app.route("/customer_list/<int:page>/", methods=['GET', 'POST'])
+@app.route("/customer_list/<int:page>/")
 def customer_list_page(page):
     service_user = ServiceUser()
     service_store = ServiceStore()
@@ -38,44 +38,43 @@ def customer_list_page(page):
     search = {'store': '', 'start_date': '', 'end_date': '',
               'row_block': service_user.get_row_block()}
 
-    if request.method == 'POST':
-        store = request.form['store']
-        start_date = request.form['start_date']
-        end_date = request.form['end_date']
-        row_block = request.form['row_block']
+    store = request.args.get('store', '')
+    start_date = request.args.get('start_date', '')
+    end_date = request.args.get('end_date', '')
+    row_block = request.args.get('row_block', '')
 
-        if store != '':
-            search['store'] = uuid.UUID(store)
-        if start_date != '':
-            search['start_date'] = start_date
-        if end_date != '':
-            search['end_date'] = end_date
-        if row_block != '':
-            search['row_block'] = int(row_block)
+    if store != '':
+        search['store'] = uuid.UUID(store)
+    if start_date != '':
+        search['start_date'] = start_date
+    if end_date != '':
+        search['end_date'] = end_date
+    if row_block != '':
+        search['row_block'] = int(row_block)
 
-        # ROW BLOCK 변경
-        if search['row_block'] != '':
-            service_user.set_row_block(search['row_block'])
+    # ROW BLOCK 변경
+    if search['row_block'] != '':
+        service_user.set_row_block(search['row_block'])
 
-            message = "조회 수가 " + row_block + "개로 변경되었습니다."
-            flash(message, "success")
+        message = "조회 수가 " + row_block + "개로 변경되었습니다."
+        flash(message, "success")
 
-        # 매장 FILTER 변경
-        if search['store'] != '' and search['start_date'] != '' and \
-                search['end_date'] != '':
-            retention = service_store.get_retention_percentage(
-                search['store'],
-                search['start_date'],
-                search['end_date']
-            )
-            order_count = service_store.order_count
-        elif search['store'] == '' or search['start_date'] == '' or \
-                search['end_date'] == '':
-            flash("조회하실 데이터를 모두 입력해주십시오", "warning")
+    # 매장 FILTER 변경
+    if search['store'] != '' and search['start_date'] != '' and \
+            search['end_date'] != '':
+        retention = service_store.get_retention_percentage(
+            search['store'],
+            search['start_date'],
+            search['end_date']
+        )
+        order_count = service_store.order_count
+    elif search['store'] == '' or search['start_date'] == '' or \
+            search['end_date'] == '':
+        flash("조회하실 데이터를 모두 입력해주십시오", "warning")
 
     users = service_user.get_customer_list(page)
     count = service_user.get_count()
-    pagination = service_user.get_pagination("/customer_list", page)
+    pagination = service_user.get_pagination("/customer_list", page, search)
     stores = service_store.get_store_list()
 
     return render_template("customer_list.html", users=users,
